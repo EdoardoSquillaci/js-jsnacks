22562cc63130c557461aa7d4bcb022b857c4ce20
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-17 00:02:42 +0200

    Primo Snack Finito

---
+++ snack-5/docu1.js
@@ -0,0 +1,20 @@
+
+let numero = []
+
+
+for (let i = 0 ; i < 6 ; i++){
+
+const input = prompt('Inserisci qui il tuo numero')
+
+
+if (input % 2 !== 0){
+  
+
+numero.push(input)
+
+console.log(numero)
+
+}
+
+
+}
