24d66ae4e67c2b6b59c7cc99cf80b2e1c06d3ffb
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-17 00:31:07 +0200

    Fine Sesto Snack

---
+++ snack-6/docu2.js
@@ -0,0 +1,22 @@
+
+
+
+let input = Number(prompt('Inserisci un numero, se è uguale a quello generato dal pc vinci 100000milioni di pesos'))
+
+console.log('il numero che hai generato è:', input)
+
+
+let numeroGenerato =Math.floor(Math.random() * 10);
+
+console.log('Il computer ha generato:' , numeroGenerato)
+
+
+
+if(input==numeroGenerato){
+
+    console.log('Hai vinto!')
+    
+
+}else if (input !== numeroGenerato){
+    console.log ('Hai Perso, riprova.')
+}
