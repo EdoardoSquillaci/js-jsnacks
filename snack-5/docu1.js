2a5b80bbfaddb631ae325d9231b8c0106c09813e
EdoardoSquillaci <edoardosquillaci@gmail.com>
2024-05-17 00:03:47 +0200

    Correzione e fine primo snack

--- snack-5/docu1.js
+++ snack-5/docu1.js
@@ -1,17 +1,19 @@
-
+//creo array vuoto
 let numero = []
 
-
+// creo ciclo per cui il prompt viene richiesto 6 volte
 for (let i = 0 ; i < 6 ; i++){
 
 const input = prompt('Inserisci qui il tuo numero')
 
-
+// creo la mia condizione per cui i numeri inseriti se dispari... 
 if (input % 2 !== 0){
   
-
+// vanno pushati nella variabile let...
 numero.push(input)
 
+
+//per poi essere stampati in console
 console.log(numero)
 
 }
